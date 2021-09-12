ba63a97958842bfda35c2e4e5c6b98dc9b34d933
Curtis <curtisnasis@gmail.com>
2021-09-12 16:41:14 -0400

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+# The dot before models means current directory or current application. 
+# Both views.py and models.py are in the same directory. This means we can use . and the name of the file (without .py). 
+# Then we import the name of
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
